d0bc2bbf0e50217977e148ba54e76b6a1aec51d3
Zaur <zaurik87@gmail.com>
2024-08-20 21:56:08 +0400

    lesson1
    applicationContext.xml
    class  ClassPathXmlApplicationContext

--- spring-app1/src/main/java/az/babayev/springcourse/TestBean.java
+++ spring-app1/src/main/java/az/babayev/springcourse/TestBean.java
@@ -2,5 +2,20 @@ package az.babayev.springcourse;
 
 public class TestBean {
 
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 
 }
