1ff517515edd813e782b202d79827868c81d5346
Zaur <zaurik87@gmail.com>
2024-08-21 07:50:53 +0400

    DI
    setMusic() -set metodu vasitəsilə Dİ
    value Some name və value 50 əli ilə daxil edilir.
    <property name="music" ref="musicBean"/> - > setMetodu vasitəsilə Dİ bu zaman boş konstruktorun olması mütləqdir
     <property name="name" value="Some name"/>
       <property name="volume" value="50"/> dəyər mənimsətmək.
    
    musicPlayer. properties falyı yaradılır. bu fayılda musicPlayer kalsında olan dəyişənlərə dəyər verilir. daha sonra
     <context:property-placeholder location="classpath:musicPlayer.properties"/> kodu applicationContexdə yazılır bu kod məlumat oxunacaq fayı göstərir. Experssion languagedən istifadə edilərək dəyərlər məniməsdilir
    "${musicPlayer.name}" və "${musicPlayer.valume}"

---
+++ Lesson6DIAppPart2/src/main/java/az/babayev/springcourse/TestSpring.java
@@ -0,0 +1,22 @@
+package az.babayev.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        context.close();
+    }
+
+
+}
