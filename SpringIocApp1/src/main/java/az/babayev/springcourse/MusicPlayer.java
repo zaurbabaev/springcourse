e0006e314ad8badc0f755224757a33b3bea59474
Zaur <zaurik87@gmail.com>
2024-08-21 07:19:42 +0400

    IoC vs DI

---
+++ SpringIocApp1/src/main/java/az/babayev/springcourse/MusicPlayer.java
@@ -0,0 +1,18 @@
+package az.babayev.springcourse;
+
+public class MusicPlayer {
+    private Music music;
+
+    // IoC
+    public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
+    }
+
+
+
+
+}
---
+++ SpringIocApp1/src/main/java/az/babayev/springcourse/TestSpring.java
@@ -0,0 +1,23 @@
+package az.babayev.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+//        Music musicBean = context.getBean("musicBean", Music.class);
+
+//        MusicPlayer musicPlayer = new MusicPlayer(musicBean);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        musicPlayer.playMusic();
+
+        context.close();
+    }
+
+
+}
