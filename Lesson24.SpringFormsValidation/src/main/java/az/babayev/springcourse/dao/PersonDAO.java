d470265dded58abd5f65381249acabe2de1fea13
Zaur <zaurik87@gmail.com>
2024-08-31 18:15:40 +0400

    Lesson 24 Validation
     <div style="color:red" th:if="${#fields.hasErrors('name')}" th:errors="*{name}"></div> edit və new html-llərə validasiya əlavə olundu. Controllerin create və update metodalrına @Valid annatasiyas; 'lav' edildi, həmçinin BindingResult obyekti əlavə edildi.

---
+++ Lesson24.SpringFormsValidation/src/main/java/az/babayev/springcourse/dao/PersonDAO.java
@@ -0,0 +1,53 @@
+package az.babayev.springcourse.dao;
+
+import az.babayev.springcourse.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private static int PERSON_COUNT;
+
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PERSON_COUNT, "Tom", "Tommi", 34,"tom@xyz.az"));
+        people.add(new Person(++PERSON_COUNT, "Bob", "Bobbi", 40,"bob@xyz.az"));
+        people.add(new Person(++PERSON_COUNT, "Mike", "Mikael", 20,"mike@xyz.ru"));
+        people.add(new Person(++PERSON_COUNT, "Katy", "Kataline", 15,"katy@xyz.az"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(Integer id) {
+        return people.stream()
+                .filter(person -> person.getId().equals(id))
+                .findAny()
+                .orElse(null);
+    }
+
+
+    public void save(Person person) {
+        person.setId(++PERSON_COUNT); // formda id yoxdur ona görədə yeni person əlavə edildikdə id 1 vahid artacaq
+        people.add(person);
+    }
+
+
+    public void update(int id, Person person) {
+        Person personUpdated = show(id);
+        personUpdated.setName(person.getName());
+        personUpdated.setSurname(person.getSurname());
+        personUpdated.setAge(person.getAge());
+        personUpdated.setEmail(person.getEmail());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
+    }
+}
